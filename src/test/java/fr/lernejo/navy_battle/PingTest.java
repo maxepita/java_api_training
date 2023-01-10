6ba7534c1657e64908135d2f272228cefa404543
maxepita <maxime.jaen@epita.fr>
2023-01-10 13:46:59 +0100

    new ping test

--- src/test/java/fr/lernejo/navy_battle/PingTest.java
+++ src/test/java/fr/lernejo/navy_battle/PingTest.java
@@ -5,21 +5,33 @@ import org.junit.jupiter.api.Test;
 
 import java.io.IOException;
 import java.net.URI;
+import java.net.URISyntaxException;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
 
 class PingTest {
     @Test
-    public void testPingRequestOnServerShouldReturn200() throws IOException, InterruptedException {
-        Server server = new Server(9876);
-        HttpResponse<String> resp = null;
-        HttpRequest requestGet = HttpRequest.newBuilder()
-            .uri(URI.create("http://localhost:9876/ping"))
-            .setHeader("Accept", "application/json")
-            .build();
-        resp =   HttpClient.newHttpClient().send(requestGet, HttpResponse.BodyHandlers.ofString());
-        Assertions.assertEquals(200, resp.statusCode());
-        HttpClient.newHttpClient().send(requestGet, HttpResponse.BodyHandlers.ofString());
+    void ping() {
+        Server server = null;
+        try {
+            server = new Server(9696);
+        } catch (Exception e) {
+            Assertions.assertEquals(0, 1, "Number of exception on server");
+        }
+        try {
+            HttpClient cli = HttpClient.newHttpClient();
+            HttpRequest requetePost = HttpRequest.newBuilder()
+                .uri(new URI("http://localhost:9696/ping"))
+                .setHeader("Accept", "application/json")
+                .setHeader("Content-Type", "application/json")
+                .POST(HttpRequest.BodyPublishers.ofString("Ping"))
+                .build();
+            HttpResponse<String> response = cli.send(requetePost, HttpResponse.BodyHandlers.ofString());
+            Assertions.assertEquals(200, response.statusCode(), "Status code 200 was expected for a POST on /ping");
+            Assertions.assertEquals("OK", response.body(), "OK body was expected for a POST  on /ping");
+        } catch (InterruptedException | IOException | URISyntaxException e) {
+            Assertions.assertEquals(0, 1, "Number of exception for POST on /ping");
+        }
     }
 }
