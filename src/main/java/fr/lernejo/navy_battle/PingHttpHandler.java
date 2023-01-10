7b27730bd4e7cc27af59341743d8584819c428b1
maxepita <maxime.jaen@epita.fr>
2023-01-10 12:07:43 +0100

    Part 3 local changer

--- src/main/java/fr/lernejo/navy_battle/PingHttpHandler.java
+++ src/main/java/fr/lernejo/navy_battle/PingHttpHandler.java
@@ -1,20 +1,22 @@
+
 package fr.lernejo.navy_battle;
+
 import com.sun.net.httpserver.HttpExchange;
 import com.sun.net.httpserver.HttpHandler;
+
 import java.io.IOException;
-import java.io.OutputStream;
+import java.io.UncheckedIOException;
 
-public class PingHttpHandler implements HttpHandler{
-
-    private final int HTTP_OK_STATUS = 200;
-    private final int HTTP_NOT_FOUND_STATUS = 404;
+public class PingHttpHandler implements HttpHandler {
 
     @Override
-    public void handle(HttpExchange exchange) throws IOException {
-        String body = "OK";
-        exchange.sendResponseHeaders(HTTP_OK_STATUS, body.length());
-        try (OutputStream os = exchange.getResponseBody()){
-            os.write(body.getBytes());
+    public void handle(HttpExchange httpExchange) {
+        try {
+            httpExchange.sendResponseHeaders(200, "OK".length());
+            httpExchange.getResponseBody().write("OK".getBytes());
+            httpExchange.getResponseBody().close();
+        } catch (IOException e) {
+            throw new UncheckedIOException(e);
         }
     }
 }
